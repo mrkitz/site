1efb071eda04bd7942a5c6d50d3ce842f97d5405
John Kitzmiller <8507977+mrkitz@users.noreply.github.com>
2026-02-17 21:16:30 -0500

    Second pandoc site

--- nav.js
+++ nav.js
@@ -1,16 +1,26 @@
 const toggle = document.querySelector('.menu-toggle');
 const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
 
 toggle.addEventListener('click', () => {
   sidebar.classList.toggle('open');
-
   if (sidebar.classList.contains('open')) {
-    sidebar.scrollIntoView({behavior: 'smooth' });
-  }
+  sidebar.scrollIntoView({ behavior: 'smooth' });
+}
+  overlay.classList.toggle('open');
+});
 
+// Close when clicking overlay
+overlay.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+  overlay.classList.remove('open');
+});
+
+// Close when clicking a TOC link
 document.querySelectorAll('#toc a').forEach(link => {
   link.addEventListener('click', () => {
     sidebar.classList.remove('open');
+    overlay.classList.remove('open');
   });
 });
-});
+
